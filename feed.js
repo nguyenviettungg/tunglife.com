efc54b39a952d9c30c1041a68c0d4c518289b32d
nguyenviettungg <nguyentung696969@gmail.com>
2019-12-12 14:10:03 +0700

    them trang feedback

---
+++ feed.js
@@ -0,0 +1,19 @@
+$(function() {
+	new WOW().init();
+	$(window).scroll(function(event) {
+ 		// var vitrimenuduoi = $('.phanchuto .text').offset().top;
+ 		// var vitrimuitenlen = $('.nutshowmenu').offset().top;
+ 		var vitri = $('html,body').scrollTop();
+ 		if (vitri >= 500 )
+ 		{	
+ 			$('.phanmenuduoi .navbar').addClass('hienthiramanhinh');
+	 		$('.phanmenuduoi .navbar').addClass('navbar-fixed-top maunenden fadeInDown animated');
+ 	 		$('.phanmenuduoi ul').addClass('divaotrong');	
+ 		}
+ 		else {
+ 			$('.phanmenuduoi .navbar').removeClass('hienthiramanhinh');
+ 			$('.phanmenuduoi .navbar').removeClass('navbar-fixed-top maunenden fadeInDown animated');
+ 			$('.phanmenuduoi ul').removeClass('divaotrong');
+ 		}
+ 	});
+});
